56e6460af4a7c2fdf1b41732d73310f5dc95d205
Micke Lisinge <me@mike.gg>
2016-04-12 09:50:45 +0200

    Move wal setup to it's own file instead of duplicating docker-entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -27,7 +27,7 @@ RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 ADD fix-acl.sh /docker-entrypoint-initdb.d/
 ADD setup-wale.sh /docker-entrypoint-initdb.d/
 
-COPY docker-entrypoint.sh /docker-entrypoint.sh
-RUN chmod +x /docker-entrypoint.sh
+COPY wal-entrypoint.sh /wal-entrypoint.sh
 
+ENTRYPOINT []
 CMD ["/usr/bin/supervisord", "-c", "/supervisord.conf"]
---
+++ wal-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# Set up wal-e
+if [ "$AWS_ACCESS_KEY_ID" = "" ]
+then
+    echo "AWS_ACCESS_KEY_ID does not exist"
+else
+    if [ "$AWS_SECRET_ACCESS_KEY" = "" ]
+    then
+        echo "AWS_SECRET_ACCESS_KEY does not exist"
+    else
+        if [ "$WALE_S3_PREFIX" = "" ]
+        then
+            echo "WALE_S3_PREFIX does not exist"
+        else
+          mkdir -p /etc/wal-e.d/env
+
+          echo "$AWS_SECRET_ACCESS_KEY" > /etc/wal-e.d/env/AWS_SECRET_ACCESS_KEY
+          echo "$AWS_ACCESS_KEY_ID" > /etc/wal-e.d/env/AWS_ACCESS_KEY_ID
+          echo "$WALE_S3_PREFIX" > /etc/wal-e.d/env/WALE_S3_PREFIX
+          chown -R postgres /etc/wal-e.d
+          su - postgres -c "crontab -l | { cat; echo \"0 3 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e backup-push $PGDATA\"; } | crontab -"
+          su - postgres -c "crontab -l | { cat; echo \"0 4 * * * /usr/bin/envdir /etc/wal-e.d/env /usr/local/bin/wal-e delete --confirm retain 7\"; } | crontab -"
+        fi
+    fi
+fi
+# End
+
+./docker-entrypoint.sh $@
